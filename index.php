b7a92ec09dbd3f84af6f1dc2e0f9a5e38ef0bb7a
LetsGaming <bergmanncrafter@gmail.com>
2023-05-04 00:29:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 <body>
     <h1>QR-Code Generator zum bestellen mit unseren Roboter </h1>
-    <h2>Bitte halten Sie ihr Smartphone bereit </h2>
+    <h2>Bitte halten Sie Ihr Smartphone bereit </h2>
     <form id="generate-form">
         <label for="tableSelect">Tischnummer:</label>
         <select name="tableNo" id="tableSelect">
